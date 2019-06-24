b861aa1e3a915398f0da5bd48f94e57dc54e1ca6
Hashim-Jones <has112@csiro.au>
2019-06-24 10:11:57 +1000

    init

---
+++ src/components/Eon.vue
@@ -0,0 +1,28 @@
+<template>
+    <div id="eon-container" :style="style">
+        <span class='vertical-heading'>{{eonName}}</span>
+    </div>
+</template>
+<script>
+    export default {
+        name: 'Eon', 
+        props: {
+            eonName: String,
+            color: String
+        },
+        computed: {
+            style() {
+                return 'background-color: ' + this.color
+            }
+        }
+    }
+</script>
+<style scoped>
+    #eon-container{
+        border: solid black 1px;
+    }
+    .vertical-heading {
+        writing-mode: vertical-rl;
+        text-orientation: mixed
+    }
+</style>
---
+++ src/components/Graphic.vue
@@ -0,0 +1,38 @@
+<template>
+    <div id="container">
+        <!-- 'y' must increase and must be the previous 'y' + previous 'height' -->
+        <svg width="100%" height="100%" version="1.1" xmlns="http://www.w3.org/2000/svg">
+            <rect class="eon" id="Phanerozoic" fill="#A1D4E2" x="0" y="0" width="100%" height="100%" stroke="black" stroke-width="5"/>
+                <rect class="era" id="Cenozoic" fill="#F6EB3B" x="10%" y="0" width="100%" height="16.67%"/>
+                <rect class="era" id="Mesozoic" fill="#6CC0DB" x="10%" y="16.67%" width="100%" height="16.67%"/>
+                <rect class="era" id="Paleozoic" fill="#A3BC99" x="10%" y="33.33%" width="100%" height="16.67%"/>
+                <rect class="era" id="Proterozoic" fill="#EA5B70" x="10%" y="50%" width="100%" height="16.67%"/>
+                <rect class="era" id="Archean" fill="#E72582" x="10%" y="66.67%" width="100%" height="16.67%"/>
+                <rect class="era" id="Hadean" fill="#B12A7E" x="10%" y="83.34%" width="100%" height="100%"/>
+        </svg>
+
+    </div>
+</template>
+<script>
+export default {
+    name: 'graphic',
+
+}
+</script>
+<style scoped>
+    #container {
+        border: solid black 1px;
+        max-width: 75%;
+        margin: auto;
+        height: 3000px;;
+    }
+    svg{
+        background-color: red;
+    }
+    rect{
+        stroke-width: 1;
+        stroke: black;
+    }
+</style>
+
+

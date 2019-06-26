ad11fef98ddc78b8a34cf1d01f27fbcf65d05fef
Hashim-Jones <has112@csiro.au>
2019-06-26 11:34:22 +1000

    Graphic now includes appropriate segments for eons, eras and periods.

--- src/components/Graphic.vue
+++ src/components/Graphic.vue
@@ -225,52 +225,68 @@
                return counter
             },
             preprocessPositions: function(data, ageHeight){
-                console.log(ageHeight)
-                var dataSizes = {}
-                //for (var item in data){
-                //    dataSizes[data[item]['id']] = data[item]
-                //}
                 var yPositionAge = 0
                 var toUpdateHeight = []
-                var currentEon, currentEra, currentPeriod, currentEpoch, currentAge
+                var lastEon, lastEra, lastPeriod, pastType
+                const elementOrder = { 
+                    'eon': 0, 
+                    'era': 1, 
+                    'period': 2, 
+                    'epoch': 3, 
+                    'epoch sub-epoch': 4, 
+                    'age': 5
+                }
+                var precambrian = false
                 for (let item in data){
+                    if (data[item]['type'] != [pastType] && elementOrder[data[item]['type']] < elementOrder[pastType]){
+                        if (data[item]['type'] == 'period'){
+                            toUpdateHeight.push([lastPeriod, (yPositionAge - parseFloat(data[lastPeriod]['y'])) + '%'])
+                        } else if (data[item]['type'] == 'era'){
+                            if (!precambrian) toUpdateHeight.push([lastPeriod, (yPositionAge - parseFloat(data[lastPeriod]['y'])) + '%'])
+                            toUpdateHeight.push([lastEra, (yPositionAge - parseFloat(data[lastEra]['y'])) + '%'])
+                        } else if (data[item]['type'] == 'eon'){
+                            if (!precambrian) toUpdateHeight.push([lastPeriod, (yPositionAge - parseFloat(data[lastPeriod]['y'])) + '%'])
+                            toUpdateHeight.push([lastEra, (yPositionAge - parseFloat(data[lastEra]['y'])) + '%'])
+                            toUpdateHeight.push([lastEon, (yPositionAge - parseFloat(data[lastEon]['y'])) + '%'])
+                        }
+                    }
+                    if (data[item]['id'] == "Precambrian") precambrian = true // HATE this, but necessary as precambrian eon has no periods
                     if (data[item]['type'] == 'age'){
-                        //currentAge = data[item]
                         data[item]['x'] = "60%"
                         data[item]['y'] = yPositionAge + '%'
                         data[item]['height'] = ageHeight + '%'
                         data[item]['width'] = "40%"
                         yPositionAge = yPositionAge + ageHeight
                     }else if (data[item]['type'] == 'epoch'){
-                        //currentEpoch = data[item]
                         data[item]['x'] = "30%"
                         data[item]['y'] = yPositionAge + '%'
                         data[item]['height'] = ageHeight * data[item]['narrow'].length + '%'
                         data[item]['width'] = "70%"
                     }else if (data[item]['type'] == 'period'){
-                        //currentPeriod = data[item]
+                        lastPeriod = item
                         data[item]['x'] = "20%"
                         data[item]['y'] = yPositionAge + '%'
-                        //console.log(data[item]['id'], this.calculateAges(dataSizes, data[item]['id']))
-                        //data[item]['height'] = this.calculateAges(dataSizes, data[item]['id']) * ageHeight + '%'
                         data[item]['width'] = "80%"
-                        console.log(currentPeriod)
                     }else if (data[item]['type'] == 'era'){
-                        //currentEra = data[item]
+                        lastEra = item
                         data[item]['x'] = "10%"
                         data[item]['y'] = yPositionAge + '%'
-                        //data[item]['height'] = this.calculateAges(dataSizes, data[item]['id']) * ageHeight + '%'
                         data[item]['width'] = "90%"
                     }else if (data[item]['type'] == 'eon'){
-                        //currentEon = data[item]
+                        lastEon = item
                         data[item]['x'] = "0%"
                         data[item]['y'] = yPositionAge + '%'
-                        //console.log(data[item]['id'], this.calculateAges(dataSizes, data[item]['id']))
-                        //data[item]['height'] = this.calculateAges(dataSizes, data[item]['id']) * ageHeight + '%'
                         data[item]['width'] = "100%"
                     }
+                    pastType = data[item]['type']
+                    if (item == data.length - 1){
+                        toUpdateHeight.push([lastEon, (yPositionAge - parseFloat(data[lastEon]['y'])) + '%'])
+                    }
                 }
-                console.log(data)
+                for (let item in toUpdateHeight){
+                    data[toUpdateHeight[item][0]]['height'] = toUpdateHeight[item][1]
+                    
+                }
                 return data
             }
         },

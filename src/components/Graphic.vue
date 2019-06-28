96eaa4b9892e394a894bbf56bb84743cf712db87
Hashim-Jones <has112@csiro.au>
2019-06-28 15:02:56 +1000

    Updated Data files and chart to more accurately reflect the v2018/08 version of the ICS (most recent)

--- src/components/Graphic.vue
+++ src/components/Graphic.vue
@@ -1,6 +1,16 @@
 <template>
     <div id="container">
-        <svg width="100%" height="100%" version="1.1" xmlns="http://www.w3.org/2000/svg">
+        <svg width="100%" :height="'40px'" version="1.1" xmlns="http://www.w3.org/2000/svg">
+            <g class='headings'>
+                <text x="5%" y="50%">Supereon</text>
+                <text x="15%" y="50%"><tspan x="15%" y="30%">Eonothem /</tspan><tspan x="15%" y="70%">Eon</tspan></text>
+                <text x="25%" y="50%"><tspan x="25%" y="30%">Erathem /</tspan><tspan x="25%" y="70%">Era</tspan></text>
+                <text x="35%" y="50%"><tspan x="35%" y="30%">System /</tspan><tspan x="35%" y="70%">Period</tspan></text>
+                <text x="55%" y="50%">Series / Epoch</text>
+                <text x="85%" y="50%">Stage / Age</text>
+            </g>
+        </svg>
+        <svg class="animated-svg" width="100%" height="100%" version="1.1" xmlns="http://www.w3.org/2000/svg">
             <g v-for="item in entries" v-bind:key="item.id" :id="item.id" @click='sendClickToEventBus'>
                 <rect  :class="item.type" :fill="item.fill" :width="item.width" :stroke="'black'" :height="item.height" :x="item.x" :y="item.y"/>
                 <text  :class="item.type" :x="item.xlabel" :y="(parseFloat(item.y) +  parseFloat(item.height) /2) + '%'">{{(item.name.slice(-4) == "Null")? "" : item.name }}</text>
@@ -12,14 +22,23 @@
     import data from '../assets/timeline_data.json'
     import EventBus from '../assets/event-bus.js'
     
-    //console.log(data)
     export default {
         name: 'graphic',
         methods: {
             calculateAgeHeight: function (data){
                 var counter = 0
+                var precambrian = false
+                var archean = false
                 for (let item in data){
-                    if (data[item]['type'] == 'age'){
+                    if (data[item]['id'] == "Precambrian"){
+                        precambrian = true
+                    } else if (data[item]['id'] == "Archean"){
+                        archean = true
+                    }
+                    if (!precambrian && data[item]['type'] == 'age'){
+                        counter++
+                    } else if ((precambrian && data[item]['type'] == 'period') || (archean && data[item]['type'] == 'era')){
+                        counter++
                         counter++
                     }
                 }
@@ -31,16 +50,18 @@
             preprocessPositions: function(data, ageHeight){
                 var yPositionAge = 0
                 var toUpdateHeight = []
-                var lastEon, lastEra, lastPeriod, pastType
+                var lastSuperEon, lastEon, lastEra, lastPeriod, pastType
                 const elementOrder = { 
-                    'eon': 0, 
-                    'era': 1, 
-                    'period': 2, 
-                    'epoch': 3, 
-                    'epoch sub-epoch': 4, 
-                    'age': 5
+                    'super-eon':0,
+                    'eon': 1, 
+                    'era': 2, 
+                    'period': 3, 
+                    'epoch': 4, 
+                    'epoch sub-epoch': 5, 
+                    'age': 6
                 }
                 var precambrian = false
+                var archean = false
                 for (let item in data){
                     if (data[item]['type'] != [pastType] && elementOrder[data[item]['type']] < elementOrder[pastType]){
                         if (data[item]['type'] == 'period'){
@@ -52,48 +73,73 @@
                             if (!precambrian) toUpdateHeight.push([lastPeriod, (yPositionAge - parseFloat(data[lastPeriod]['y'])) + '%'])
                             toUpdateHeight.push([lastEra, (yPositionAge - parseFloat(data[lastEra]['y'])) + '%'])
                             toUpdateHeight.push([lastEon, (yPositionAge - parseFloat(data[lastEon]['y'])) + '%'])
+                        }else if (data[item]['type'] == 'super-eon'){
+                            if (!precambrian) toUpdateHeight.push([lastPeriod, (yPositionAge - parseFloat(data[lastPeriod]['y'])) + '%'])
+                            toUpdateHeight.push([lastEra, (yPositionAge - parseFloat(data[lastEra]['y'])) + '%'])
+                            toUpdateHeight.push([lastEon, (yPositionAge - parseFloat(data[lastEon]['y'])) + '%'])
                         }
                     }
-                    if (data[item]['id'] == "Precambrian") precambrian = true // HATE this, but necessary as precambrian eon has no periods
+                    if (data[item]['id'] == "Precambrian") {// HATE this, but necessary as precambrian eon has no periods
+                        precambrian = true
+                    } 
+                    if (data[item]['id'] == "Archean") {// HATE this, but necessary as precambrian eon has no periods
+                        archean = true
+                    } 
                     if (data[item]['type'] == 'age'){
-                        data[item]['x'] = "60%"
+                        data[item]['x'] = "70%"
                         data[item]['y'] = yPositionAge + '%'
                         data[item]['height'] = ageHeight + '%'
-                        data[item]['width'] = "40%"
-                        data[item]['xlabel'] = "80%"
+                        data[item]['width'] = "30%"
+                        data[item]['xlabel'] = "85%"
                         yPositionAge = yPositionAge + ageHeight
                     }else if (data[item]['type'] == 'epoch sub-epoch'){
-                        data[item]['x'] = "45%"
+                        data[item]['x'] = "55%"
                         data[item]['y'] = yPositionAge + '%'
                         data[item]['height'] = ageHeight * data[item]['narrow'].length + '%'
-                        data[item]['width'] = "55%"
-                        data[item]['xlabel'] = "52.5%"
+                        data[item]['width'] = "45%"
+                        data[item]['xlabel'] = "62.5%"
                     }else if (data[item]['type'] == 'epoch'){
-                        data[item]['x'] = "30%"
+                        data[item]['x'] = "40%"
                         data[item]['y'] = yPositionAge + '%'
                         data[item]['height'] = ageHeight * data[item]['narrow'].length + '%'
-                        data[item]['width'] = "70%"
-                        data[item]['xlabel'] = (data[item]['id'] == 'Mississippian' || data[item]['id'] == 'Pennsylvanian') ? "37.5%" : "45%"
+                        data[item]['width'] = "60%"
+                        data[item]['xlabel'] = (data[item]['id'] == 'Mississippian' || data[item]['id'] == 'Pennsylvanian') ? "47.5%" : "55%"
                     }else if (data[item]['type'] == 'period'){
                         lastPeriod = item
+                        data[item]['x'] = "30%"
+                        data[item]['y'] = yPositionAge + '%'
+                        data[item]['width'] = "70%"
+                        data[item]['xlabel'] = "35%"
+                        if (precambrian){
+                            data[item]['height'] = ageHeight*2 + '%'
+                            yPositionAge = yPositionAge + ageHeight*2
+                            data[item]['xlabel'] = "65%"
+                        }
+                    }else if (data[item]['type'] == 'era'){
+                        lastEra = item
                         data[item]['x'] = "20%"
                         data[item]['y'] = yPositionAge + '%'
                         data[item]['width'] = "80%"
-                        data[item]['xlabel'] = "25%"
-                    }else if (data[item]['type'] == 'era'){
-                        lastEra = item
+                        data[item]['xlabel'] =  (!precambrian) ? "25%" : "25%"
+                        if (archean){
+                            data[item]['height'] = ageHeight*2 + '%'
+                            yPositionAge = yPositionAge + ageHeight*2
+                            data[item]['xlabel'] = "60%"
+                        }
+                    }else if (data[item]['type'] == 'eon'){
+                        lastEon = item
                         data[item]['x'] = "10%"
                         data[item]['y'] = yPositionAge + '%'
                         data[item]['width'] = "90%"
-                        data[item]['xlabel'] =  (!precambrian) ? "15%" : "20%"
-                        // Making Hadean Era the Width of 5 Ages
+                        data[item]['xlabel'] = "15%"
+                        // Making Hadean Eon the Height of 5 Ages
                         if (precambrian && data[item]['id'] == 'Hadean') {
                             data[item]['height'] = ageHeight * 5 + "%"
                             yPositionAge = yPositionAge + (ageHeight * 5)
                             data[item]['xlabel'] =  "55%"
                         }
-                    }else if (data[item]['type'] == 'eon'){
-                        lastEon = item
+                    }else if (data[item]['type'] == 'super-eon'){
+                        lastSuperEon = item
                         data[item]['x'] = "0%"
                         data[item]['y'] = yPositionAge + '%'
                         data[item]['width'] = "100%"
@@ -101,8 +147,15 @@
                     }
                     pastType = data[item]['type']
                     if (item == data.length - 1){
-                        toUpdateHeight.push([lastEon, (yPositionAge - parseFloat(data[lastEon]['y'])) + '%'])
+                        toUpdateHeight.push([lastSuperEon, (yPositionAge - parseFloat(data[lastSuperEon]['y'])) + '%'])
                     }
+                    /*
+                    if (precambrian && data[item]['type'] != 'age' && data[item]['type'] == [pastType]){
+                        toUpdateHeight.push([item, ageHeight + "%"])
+                        yPositionAge = yPositionAge + ageHeight
+                    }
+                    */
+
                 }
                 for (let item in toUpdateHeight){
                     data[toUpdateHeight[item][0]]['height'] = toUpdateHeight[item][1]
@@ -113,6 +166,7 @@
         },
         data () {
             return {
+                dividerPosition: -1,
                 ageHeight: this.calculateAgeHeight(data),
                 entries: this.preprocessPositions(data, this.calculateAgeHeight(data))
             }
@@ -121,12 +175,13 @@
 </script>
 <style scoped>
     #container {
+        position: relative;
         border: solid white 1px;
         width: 75%;
         min-width: 980px;
         max-width: 1250px;
         margin: auto;
-        height: 3000px;;
+        height: 3500px;
     }
     svg{
         background-color: white;
@@ -146,7 +201,7 @@
         top: 0px;
         transition: transform .2s ease-in-out;
     }
-    g:hover{
+    .animated-svg g:hover{
         transform: translate3d(-2.4%, 0px, 0px);
         cursor: pointer;
     }

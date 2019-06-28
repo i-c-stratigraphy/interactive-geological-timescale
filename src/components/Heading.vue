df110794524292e4ef1c00fc7fd6973653754a1e
Hashim-Jones <has112@csiro.au>
2019-06-28 13:00:04 +1000

    Modified header/heading.

--- src/components/Heading.vue
+++ src/components/Heading.vue
@@ -1,5 +1,9 @@
 <template>
-    <h1>INTERNATIONAL CHRONOSTRATIGRAPHIC CHART</h1>
+    <div id="header">
+        <h1><span id="smaller-heading">INTERACTIVE</span><br>INTERNATIONAL CHRONOSTRATIGRAPHIC CHART</h1>
+        <p>An interactive version of the <a href="http://www.stratigraphy.org/index.php/ics-chart-timescale">International Chronostratigraphic Chart</a>, based on it's <a href="https://www.w3.org/standards/semanticweb/">Semantic Web</a> representation.</p>
+        <p>For comments/issues and this visualisation's code, please see its <a href="https://github.com/CSIRO-enviro-informatics/interactive-geological-timescale">Git Code Repository</a>.</p><br>
+    </div>
 </template>
 
 <script>
@@ -23,5 +27,8 @@ export default {
     }
     a {
         color: #42b983;
+    }
+    #smaller-heading{
+        font-size: 0.8em;
     }
 </style>

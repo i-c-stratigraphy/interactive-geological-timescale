9502b3da9145c68f8a39e19c9d2333ed6868e88e
Hashim-Jones <has112@csiro.au>
2019-07-05 15:05:14 +1000

    Import tooltips vuejs extension.

--- src/main.js
+++ src/main.js
@@ -3,6 +3,11 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+// Import vue-directive-tooltip module
+import Tooltip from 'vue-directive-tooltip';
+import 'vue-directive-tooltip/dist/vueDirectiveTooltip.css';
+Vue.use(Tooltip);
+
 new Vue({
   render: h => h(App),
 }).$mount('#app')
